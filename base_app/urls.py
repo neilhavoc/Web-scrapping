015123b160470ab76b0f16093b917e41d882f670
neilhavoc <sarioneiljohm@gmail.com>
2023-03-28 17:47:00 +0800

    first commit

---
+++ base_app/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import include, path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),  
+    path('addnew',views.addnew, name='addnew'),  
+    path('edit/<int:id>', views.edit, name='edit'),  
+    path('update/<int:id>', views.update, name='update'),  
+    path('delete/<int:id>', views.destroy, name='destroy'),  
+]
---
+++ base_app/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import redirect, render
+from django.http import HttpResponse
+from base_app.forms import EmployeeForm
+from base_app.models import Employee 
+# Create your views here.  
+def addnew(request):  
+    if request.method == "POST":  
+        form = EmployeeForm(request.POST)  
+        if form.is_valid():  
+            try:  
+                form.save()  
+                return redirect('/')  
+            except:  
+                pass
+    else:  
+        form = EmployeeForm()  
+    return render(request,'base/index.html',{'form':form})  
+ 
+def index(request):  
+    employees = Employee.objects.all()  
+    return render(request,"base/show.html",{'employees':employees})  
+ 
+def edit(request, id):  
+    employee = Employee.objects.get(id=id)  
+    return render(request,'base/edit.html', {'employee':employee})  
+ 
+def update(request, id):  
+    employee = Employee.objects.get(id=id)  
+    form = EmployeeForm(request.POST, instance = employee)  
+    if form.is_valid():  
+        form.save()  
+        return redirect("/")  
+    return render(request, 'base/edit.html', {'employee': employee})  
+     
+def destroy(request, id):  
+    employee = Employee.objects.get(id=id)  
+    employee.delete()  
+    return redirect("/")
