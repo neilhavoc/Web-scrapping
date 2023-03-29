18abedff11b27296369d8ce50007d027c275ad30
neilhavoc <sarioneiljohm@gmail.com>
2023-03-29 16:46:23 +0800

    add new user modal

--- base_app/urls.py
+++ base_app/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('addnew',views.addnew, name='addnew'),  
     path('edit/<int:id>', views.edit, name='edit'),  
     path('update/<int:id>', views.update, name='update'),  
-    path('delete/<int:id>', views.destroy, name='destroy'),  
+    path('delete/<int:id>', views.destroy, name='destroy'),
+    path('addnewuser', views.addnewuser, name='addnewuser'),
+    path('index', views.index, name='index'),    
 ]
--- base_app/views.py
+++ base_app/views.py
@@ -15,11 +15,45 @@ def addnew(request):
     else:  
         form = EmployeeForm()  
     return render(request,'base/index.html',{'form':form})  
- 
+
+def addnewuser(request): 
+    if request.method == "POST":  
+        form = EmployeeForm(request.POST)  
+        if form.is_valid():  
+            try:  
+                form.save()  
+                return redirect('/')  
+            except:  
+                pass
+    else:  
+        form = EmployeeForm()  
+    return render(request,'base/show.html',{'form':form})
+
 def index(request):  
-    employees = Employee.objects.all()  
-    return render(request,"base/show.html",{'employees':employees})  
+    employees = Employee.objects.all()
+
+    if request.method == "POST":  
+        form = EmployeeForm(request.POST)  
+        if form.is_valid():  
+            try:  
+                form.save()  
+                return redirect('/')   
+            except:  
+                pass
+    else:  
+        form = EmployeeForm()
+
+    return render(request,"base/show.html",{'employees':employees, 'form':form})  
  
+def updateUser(request, id):  
+    employee = Employee.objects.get(id=id)  
+    form = EmployeeForm(request.POST, instance = employee)  
+    if form.is_valid():  
+        form.save()  
+        return redirect("/")  
+     
+
+
 def edit(request, id):  
     employee = Employee.objects.get(id=id)  
     return render(request,'base/edit.html', {'employee':employee})  
